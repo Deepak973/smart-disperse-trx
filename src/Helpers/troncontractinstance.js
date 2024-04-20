f8ce6ff1adbb2bf75eeac31e70dfe4eb38f89728
Deepak973 <deepak.rj.dr@gmail.com>
2024-04-20 10:46:21 +0530

    Integrated tron contract

--- src/Helpers/troncontractinstance.js
+++ src/Helpers/troncontractinstance.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 import TronWeb from "tronweb";
 import tronabi from "../artifacts/contracts/TronContract.json";
 
-export const TronContractInstance = async() => {
-    try {
-        console.log("trying...")
+export const TronContractInstance = async () => {
+  try {
+    console.log("trying...");
     const { tronWeb } = window;
-    const TroncontractAddress = 'TGBSgXpo5rfwcMdB3KMab6j3CLBaSUFJcX';
-    let contract = await tronWeb.contract(tronabi, TroncontractAddress); 
+    const TroncontractAddress = "TSijZfgARceZzHGBU14GcfQuDSsQhZtVdh";
+    let contract = await tronWeb.contract(tronabi, TroncontractAddress);
     console.log(contract);
     return contract;
-    } catch (error) {
-        console.log("error:",error.message)
-        throw error;
-    }
-}
+  } catch (error) {
+    console.log("error:", error.message);
+    throw error;
+  }
+};
