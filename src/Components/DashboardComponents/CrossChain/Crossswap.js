9c25b531b4f6a5f329704f56553ac81e9c7b0118
Kaushal-205 <20bce123@nirmauni.ac.in>
2024-05-03 11:06:51 +0530

    token format changed

--- src/Components/DashboardComponents/CrossChain/Crossswap.js
+++ src/Components/DashboardComponents/CrossChain/Crossswap.js
@@ -75,19 +75,19 @@ function Crossswap({ activeTab }) {
       name: "Ethereum",
       tokens: [
         {
-          blockchain: "Ethereum",
-          symbol: "ETH",
-          address: null
+          "blockchain": "ETH",
+          "address": null,
+          "symbol": "ETH",
         },
         {
-          blockchain: "Ethereum",
-          symbol: "USDT",
-          address: "0xdac17f958d2ee523a2206206994597c13d831ec7"
+          "blockchain": "ETH",
+          "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+          "symbol": "USDT",
         },
         {
-          blockchain: "Ethereum",
-          symbol: "USDC",
-          address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+          "blockchain": "ETH",
+          "address": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+          "symbol": "USDC",
         },
       ],
     },
@@ -95,14 +95,14 @@ function Crossswap({ activeTab }) {
   name: "Optimism",
         tokens: [
         {
-          blockchain: "Optimism",
-          symbol: "USDT",
-          address: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58"
+          "blockchain": "Optimism",
+          "address": "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+          "symbol": "USDT",
         },
         {
-          blockchain: "Ethereum",
-          symbol: "USDC",
-          address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85"
+          "blockchain": "Optimism",
+          "address": "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+          "symbol": "USDC",
         },
       ],
     },
@@ -110,14 +110,14 @@ function Crossswap({ activeTab }) {
     name: "Polygon",
         tokens: [
         {
-          blockchain: "Polygon",
-          symbol: "USDT",
-          address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"
+          "blockchain": "Polygon",
+          "address": "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+          "symbol": "USDT",
         },
         {
-          blockchain: "Polygon",
-          symbol: "USDC",
-          address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
+          "blockchain": "Polygon",
+          "address": "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+          "symbol": "USDC",
         },
       ],
     },
@@ -125,14 +125,14 @@ function Crossswap({ activeTab }) {
       name: "Tron",
           tokens: [
           {
-            blockchain: "TRON",
-            symbol: "USDT",
-            address: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
+            "blockchain": "TRON",
+            "address": "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            "symbol": "USDT"
           },
           {
-            blockchain: "TRON",
-            symbol: "USDC",
-            address: "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8"
+            "blockchain": "TRON",
+            "address": "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8",
+            "symbol": "USDC"
           },
         ],
       },
@@ -273,6 +273,7 @@ function Crossswap({ activeTab }) {
               style={{
                 padding: "30px 20px",
                 display: "flex",
+                flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
               }}
--- src/Helpers/swapComponent.js
+++ src/Helpers/swapComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import { RangoClient } from 'rango-sdk-basic';
+import textStyle from "@/Components/DashboardComponents/SameChain/Type/textify.module.css";
 import { checkApprovalSync, checkTransactionStatusSync, prepareEvmTransaction } from './utilityFunctions';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -16,11 +17,22 @@ const SwapComponent = ({selectedFromToken,selectedToToken}) => {
   console.log(selectedToToken)
   useEffect(() => {
     const fetchQuote = async () => {
-      
+      console.log(selectedFromToken,selectedToToken);
+      console.log("in fetchquote..")
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const swapRequest = {
-        selectedFromToken,
-        selectedToToken,
+        // from: {
+        //   "blockchain": "BSC",
+        //   "symbol": "BNB",
+        //   "address": null
+        // },
+        // to: {
+        //   "blockchain": "AVAX_CCHAIN",
+        //   "symbol": "USDT.E",
+        //   "address": "0xc7198437980c041c805a1edcba50c1ce5db95118"
+        // },
+        from: selectedFromToken,
+        to: selectedToToken,
         amount: "100000000000000000",
         fromAddress: "0x2131A6c0b66bE63E38558dC5fbe4C0ab65b9906e",
         toAddress: "0x5428DAc9103799F18eb6562eD85e48E0790D4643",
@@ -29,8 +41,9 @@ const SwapComponent = ({selectedFromToken,selectedToToken}) => {
         referrerAddress: null,
         referrerFee: null,
       };
-      
+      console.log("swaprequest", swapRequest);
       const swap = await rangoClient.swap(swapRequest);
+      console.log("swaprequest", swap);
 
       if (!!swap.error || swap.resultType !== 'OK') {
         const msg = `Error swapping, message: ${swap.error}, status: ${swap.resultType}`;
@@ -42,7 +55,7 @@ const SwapComponent = ({selectedFromToken,selectedToToken}) => {
     };
 
     fetchQuote();
-  }, [selectedFromToken]);
+  }, [selectedFromToken,selectedToToken]);
 
   const handleSwap = async () => {
     console.log("swap btn clicked")
@@ -71,7 +84,7 @@ const SwapComponent = ({selectedFromToken,selectedToToken}) => {
 
   return (
     <div>
-      <button onClick={handleSwap}>Swap</button>
+      <button style={{margin:"10px"}} className={textStyle.sendbutton} onClick={handleSwap}>Swap Token</button>
     </div>
   );
 };
